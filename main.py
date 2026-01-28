b82959626570fcb279b362eafcc904e8af10cb9b
gryr6v549g-a11y <gryr6v549g@privaterelay.appleid.com>
2026-01-29 02:29:23 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram_bot import send_message
+from telegram_bot import build_message
 
 print("🤖 텔레그램 봇 실행됨")
-send_message("✅ Railway에서 텔레그램 봇 정상 작동 중")
+build_message("✅ Railway에서 텔레그램 봇 정상 작동 중")
