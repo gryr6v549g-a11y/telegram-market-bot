97f00cb3af1f3eac78bb0b6074e1a16dcaaf07c2
gryr6v549g-a11y <gryr6v549g@privaterelay.appleid.com>
2026-02-04 01:44:31 +0900

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -60,7 +60,7 @@ def market_prices():
         return close, chg, high_1m, low_1m
 
     usdkrw = asset("USDKRW=X")
-    jpykrw = asset("JPYKRW=X", fx=100)
+    jpykrw = asset("JPYKRW=X")
     usdjpy = asset("JPY=X", fx=100)
     gold = asset("GC=F")
     wti = asset("CL=F")
@@ -151,7 +151,7 @@ def build_message():
 엔/원(100엔): {fmt(jpykrw[0])} ({arrow(jpykrw[1])}{fmt(jpykrw[1])})
   · 한달: 고 {fmt(jpykrw[2])} / 저 {fmt(jpykrw[3])}
 
-엔/달러(100엔): {fmt(usdjpy[0])} ({arrow(usdjpy[1])}{fmt(usdjpy[1])})
+달러/엔: {fmt(usdjpy[0])} ({arrow(usdjpy[1])}{fmt(usdjpy[1])})
   · 한달: 고 {fmt(usdjpy[2])} / 저 {fmt(usdjpy[3])}
 
 금: {fmt(gold[0])} ({arrow(gold[1])}{fmt(gold[1])})
